cb3ea490dd90fc0337843c23b1f8be8f53af17af
damolaajayi <damolaajayi@gmail.com>
2018-10-04 23:03:40 +0100

    changes to settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,7 +139,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-os.makedirs(STATIC_ROOT, exist_ok=True)
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
         ]
