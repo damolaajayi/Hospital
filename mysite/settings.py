b3a1447763852ce15b4b10c2cc47fd4ea090aacb
damolaajayi <damolaajayi@gmail.com>
2018-10-04 12:35:27 +0100

    Changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -152,6 +152,5 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
