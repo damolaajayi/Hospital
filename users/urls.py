8d4c70e58bef5506fe6dacbdb82204c4685169ad
damolaajayi <damolaajayi@gmail.com>
2018-10-09 14:26:18 +0100

    New changes

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from django.urls import include
-
+from django.contrib.auth.views import LoginView, LogoutView
+app_name = 'users'
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path('users/', include('django.contrib.auth.urls')),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name="logout"),
 ]
