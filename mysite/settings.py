d6b9c1e8e54c637ecef4879534b278c67a2f820f
damolaajayi <damolaajayi@gmail.com>
2018-10-11 13:30:22 +0100

    changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'damola')
 EMAIL_HOST_PASSWORD = os.environ.get('davidbeckham')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['damola2018.herokuapp.com', '127.0.0.1']
